f7d28ba5ef36975a8a2edbf27c3ac894fec2440b
proxymurder <30883472+proxymurder@users.noreply.github.com>
2022-12-23 08:41:37 -0600

    working smallstep plugin

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig, loadEnv } from 'vite';
-import restart from 'vite-plugin-restart';
 import smallstep from 'vite-plugin-smallstep';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig(async ({ mode }) => {
-    process.env = {
-        ...process.env,
-        ...loadEnv(mode, process.cwd(), 'VITE_SERVER'),
-    };
-
     return {
         server: {
             host: true,
@@ -22,16 +16,9 @@ export default defineConfig(async ({ mode }) => {
             },
         },
         plugins: [
-            restart({
-                restart: [
-                    path.relative(__dirname, process.env.VITE_SERVER_CRT),
-                    'tsconfig.json',
-                ],
-            }),
             vue(),
             smallstep({
-                crt: process.env.VITE_SERVER_CRT,
-                key: process.env.VITE_SERVER_KEY,
+                path: process.env.VITE_STEPPATH,
             }),
         ],
     };
